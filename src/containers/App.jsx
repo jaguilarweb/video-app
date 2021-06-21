381166a98abfc0255f4d257fb76177f7d911dea6
Jenny Aguilar <contact@jaguilarweb.com>
2021-06-21 19:57:42 -0400

    feat: Add Footer component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,6 +4,7 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
 
 import '../assets/styles/App.scss';
 
@@ -18,6 +19,8 @@ const App = () => (
         <CarouselItem />
       </Carousel>
     </Categories>
+
+    <Footer />
   </div>
 );
 
